dfe017250c3c559e30894e272292c997fe461b51
Brendon Moncada <63375235+Brendonm17@users.noreply.github.com>
2026-01-15 07:43:18 -0800

    Add files via upload

---
+++ debug.h
@@ -0,0 +1,10 @@
+
+#ifndef btl_debug_h
+#define btl_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
